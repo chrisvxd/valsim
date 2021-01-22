df7f18d5b9274df43cfe14d2c60e712e284ee132
Chris Villa <chrisvilla@me.com>
2021-01-22 12:10:19 +0000

    feat: remove unnecessary server code

--- urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, url
-
-urlpatterns = patterns('',
-    url(r'^$', 'valsim.views.page', name='valsim_page'),
-    url(r'^image', 'valsim.views.image', name='valsim_image'),
-)
